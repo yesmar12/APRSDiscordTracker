8eb9de52332459b744610c7a6fd35b56b1172d45
ramsey <ramseybissex@gmail.com>
2017-07-23 01:49:12 -0500

    going to bed saving work to test arena

--- main.go
+++ main.go
@@ -8,12 +8,17 @@ import "net/http"
 
 func main() {
 	fmt.Println("Hello, World!")
-	transport := &http.Trasport{
-		MaxIdleConns:		10,
-		IdleConnTimeout:	20* time.Second,
-		DisableCompression:	true,
+//	transport := &http.Trasport{
+//		MaxIdleConns:		10,
+//		IdleConnTimeout:	20* time.Second,
+//		DisableCompression:	true,
+//	}
+//	client := &http.Client{Transport: transport}
+	response, err := http.Get("https://httpbin.org/get")
+	fmt.Println(response.Body)
+	if err != nil {
+		fmt.Println("there was an error :( :")
+		fmt.Println(err)
 	}
-	client := &http.Client{Transport: transport}
-	response, err := client.Get("https://test.com/api/get?name=Aggie-1")
 }
 
