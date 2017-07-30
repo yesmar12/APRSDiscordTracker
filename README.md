a0525e556d62c1a55a6de51806cdfbbf887f501d
ramsey <ramseybissex@gmail.com>
2017-07-29 22:13:15 -0500

    renamed the github in the readme, setup context for the server

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# APRSFetchDiscord
+# APRSDiscordTracker
 This is a program that will fetch APRS data from aprs.fi and then display it on discord
--- server.go
+++ server.go
@@ -4,13 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 var (
-	Token string
+	Token  string
+	Status bool
 )
 
 func init() {
@@ -55,6 +58,47 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	if m.Content == "!test" {
-		s.ChannelMessageSend(m.ChannelID, "Hello, World!")
+		s.ChannelMessageSend(m.ChannelID, "Fuck you "+m.Author.Username)
 	}
+
+	if m.Content == "!track" {
+		if Status == false {
+			messageChannel(s, m.ChannelID, "Now trackning the aggie1 with aprs.fi")
+			req, err := http.NewRequest("GET", "https://httpbin.org/ip", nil)
+
+			req.Header.Add("user-agent", "APRSDiscordTracker/0.0.1")
+
+			if err != nil {
+				fmt.Println("there was an error :( :")
+				fmt.Println(err)
+			}
+
+			response, _ := http.DefaultClient.Do(req)
+
+			defer response.Body.Close()
+			if response.StatusCode == 200 {
+				bodyBytes, err2 := ioutil.ReadAll(response.Body)
+				bodyString := string(bodyBytes)
+				messageChannel(s, m.ChannelID, bodyString)
+				if err2 != nil {
+					fmt.Println("oh god")
+				}
+			}
+
+			Status = true
+		} else {
+			messageChannel(s, m.ChannelID, "Already tracking balloon")
+		}
+		return
+	}
+
+	if m.Content == "!stoptrack" {
+		messageChannel(s, m.ChannelID, "No longer tracking balloon")
+		Status = false
+	}
+
+}
+func messageChannel(s *discordgo.Session, channelID string, message string) {
+	fmt.Println(message)
+	s.ChannelMessageSend(channelID, message)
 }
